f388076e9a6f1634edc2824d193f5876d1d1e184
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2022-05-24 08:51:15 +0200

    Introduces GatewayTimeout error

--- src/errors.rs
+++ src/errors.rs
@@ -10,6 +10,8 @@ pub enum DNSimpleError {
     Authentication,
     #[error("Message: {0}")]
     BadRequest(String, Option<Value>),
+    #[error("Message: {0}")]
+    GatewayTimeout(String),
     #[error("Transport Error – {0}({0})")]
     Transport(String, String),
     #[error("Deserialization Error {0}")]
@@ -17,18 +19,37 @@ pub enum DNSimpleError {
 }
 
 impl DNSimpleError {
-    pub fn parse_response(_code: u16, response: Response) -> DNSimpleError {
+    pub fn parse_response(code: u16, response: Response) -> DNSimpleError {
+        if code == 400 {
+            return Self::bad_request(response)
+        } else if code == 504 {
+            return Self::gateway_timeout(response)
+        }
+        else { Self::Transport("OOPS".into(), "oops".into()) }
+    }
+
+    pub fn parse_transport(_transport: Transport) -> DNSimpleError {
+        Self::Transport("VROOM".into(), "VROOM".into())
+    }
+
+    fn bad_request(response: Response) -> DNSimpleError {
         let json = response
             .into_json::<Value>()
             .map_err(|e| DNSimpleError::Deserialization(e.to_string()))
             .unwrap();
         let message = &json["message"];
-        let errors = serde::__private::Option::Some(json["errors"].borrow().clone());
+        let errors = Some(json["errors"].borrow().clone());
 
         return Self::BadRequest(message.to_string(), errors);
     }
 
-    pub fn parse_transport(_transport: Transport) -> DNSimpleError {
-        return Self::Transport("VROOM".into(), "VROOM".into());
+    fn gateway_timeout(response: Response) -> DNSimpleError {
+        let json = response
+            .into_json::<Value>()
+            .map_err(|e| DNSimpleError::Deserialization(e.to_string()))
+            .unwrap();
+        let message = &json["message"];
+
+        return Self::GatewayTimeout(message.to_string());
     }
 }
