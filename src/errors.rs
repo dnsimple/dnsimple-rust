198ced1633808710d0b0fed133c4398efbb1f4db
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2022-05-24 15:54:23 +0200

    Added more error types recognised by the client

--- src/errors.rs
+++ src/errors.rs
@@ -6,12 +6,26 @@ use ureq::{Response, Transport};
 
 #[derive(Error, Deserialize, Serialize, Debug)]
 pub enum DNSimpleError {
-    #[error("Authentication error")]
-    Authentication,
+    #[error("Authentication failed")]
+    Unauthorized,
+    #[error("Bad Gateway")]
+    BadGateway,
     #[error("Message: {0}")]
     BadRequest(String, Option<Value>),
     #[error("Message: {0}")]
     GatewayTimeout(String),
+    #[error("Method not Allowed")]
+    MethodNotAllowed,
+    #[error("Message: {0}")]
+    NotFound(String),
+    #[error("Your account is not subscribed or not in good standing")]
+    PaymentRequired,
+    #[error("Message: {0}")]
+    PreconditionRequired(String),
+    #[error("Service Unavailable")]
+    ServiceUnavailable,
+    #[error("You exceeded the allowed number of requests per hour and your request has temporarily been throttled.")]
+    TooManyRequests,
     #[error("Transport Error – {0}({0})")]
     Transport(String, String),
     #[error("Deserialization Error {0}")]
@@ -20,12 +34,18 @@ pub enum DNSimpleError {
 
 impl DNSimpleError {
     pub fn parse_response(code: u16, response: Response) -> DNSimpleError {
-        if code == 400 {
-            Self::bad_request(response)
-        } else if code == 504 {
-            Self::gateway_timeout(response)
-        } else {
-            Self::Transport("OOPS".into(), "oops".into())
+        match code {
+            400 => Self::bad_request(response),
+            401 => Self::Unauthorized,
+            402 => Self::PaymentRequired,
+            404 => Self::not_found(response),
+            405 => Self::MethodNotAllowed,
+            428 => Self::precondition_required(response),
+            429 => Self::TooManyRequests,
+            502 => Self::BadGateway,
+            503 => Self::ServiceUnavailable,
+            504 => Self::gateway_timeout(response),
+            _ => Self::Transport("OOPS".into(), "oops".into()),
         }
     }
 
@@ -34,23 +54,36 @@ impl DNSimpleError {
     }
 
     fn bad_request(response: Response) -> DNSimpleError {
-        let json = response
-            .into_json::<Value>()
-            .map_err(|e| DNSimpleError::Deserialization(e.to_string()))
-            .unwrap();
-        let message = &json["message"];
-        let errors = Some(json["errors"].borrow().clone());
+        let json = Self::response_to_json(response);
 
-        Self::BadRequest(message.to_string(), errors)
+        Self::BadRequest(
+            json["message"].to_string(),
+            Some(json["errors"].borrow().clone()),
+        )
     }
 
     fn gateway_timeout(response: Response) -> DNSimpleError {
-        let json = response
+        let json = Self::response_to_json(response);
+
+        Self::GatewayTimeout(json["message"].to_string())
+    }
+
+    fn not_found(response: Response) -> DNSimpleError {
+        let json = Self::response_to_json(response);
+
+        Self::NotFound(json["message"].to_string())
+    }
+
+    fn precondition_required(response: Response) -> DNSimpleError {
+        let json = Self::response_to_json(response);
+
+        Self::PreconditionRequired(json["message"].to_string())
+    }
+
+    fn response_to_json(response: Response) -> Value {
+        response
             .into_json::<Value>()
             .map_err(|e| DNSimpleError::Deserialization(e.to_string()))
-            .unwrap();
-        let message = &json["message"];
-
-        Self::GatewayTimeout(message.to_string())
+            .unwrap()
     }
 }
