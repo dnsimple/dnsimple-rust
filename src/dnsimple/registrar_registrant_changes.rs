90c613ac04c3c740049d5101a46e14cdc079be11
Ivan Bakalov <ibbakalov@gmail.com>
2023-12-28 12:45:48 +0200

    Domain contact management API (#51)
    This PR introduces 5 new endpoints that are part of the domain contact management API that was introduced in https://github.com/dnsimple/dnsimple-developer/pull/501.
    
    The following endpoints have been added:
    - **listRegistrantChanges**: GET `/{account}/registrar/registrant_changes`
    - **createRegistrantChange**: POST `/{account}/registrar/registrant_changes`
    - **checkRegistrantChange**: POST `/{account}/registrar/registrant_changes/check`
    - **getRegistrantChange**: GET `/{account}/registrar/registrant_changes/{registrantchange}`
    - **deleteRegistrantChange**: DELETE `/{account}/registrar/registrant_changes/{registrantchange}`
    
    Belongs to https://github.com/dnsimple/dnsimple-business/issues/1729
    Belongs to https://github.com/dnsimple/dnsimple-external-integrations/issues/18

---
+++ src/dnsimple/registrar_registrant_changes.rs
@@ -0,0 +1,193 @@
+use crate::dnsimple::registrar::Registrar;
+use crate::dnsimple::tlds::TldExtendedAttribute;
+use crate::dnsimple::{DNSimpleResponse, Endpoint, RequestOptions};
+use crate::errors::DNSimpleError;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+/// Represents the contact change data
+#[derive(Debug, Deserialize)]
+pub struct RegistrantChange {
+    /// The contact change id in DNSimple
+    pub id: u64,
+    /// The associated account ID.
+    pub account_id: u64,
+    /// The associated contact ID.
+    pub contact_id: u64,
+    /// The associated domain ID.
+    pub domain_id: u64,
+    /// The registrant change state.
+    pub state: String,
+    /// The extended attributes.
+    pub extended_attributes: Option<HashMap<String, String>>,
+    /// True if the registrant change is a registry owner change.
+    pub registry_owner_change: bool,
+    /// When the Inter-Registrar Transfer lock (60 days) is going to be lifted.
+    pub irt_lock_lifted_by: Option<String>,
+    /// When the registrant change was created in DNSimple.
+    pub created_at: String,
+    /// When the registrant change was last updated in DNSimple.
+    pub updated_at: String,
+}
+
+/// Represents the contact change check data
+#[derive(Debug, Deserialize)]
+pub struct RegistrantChangeCheck {
+    /// The associated contact ID.
+    pub contact_id: u64,
+    /// The associated domain ID.
+    pub domain_id: u64,
+    /// The extended attributes.
+    pub extended_attributes: Option<Vec<TldExtendedAttribute>>,
+    /// True if the registrant change is a registry owner change.
+    pub registry_owner_change: bool,
+}
+
+/// Payload used to check the requirements for a contact change
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RegistrantChangePayload {
+    /// The associated domain ID.
+    pub domain_id: u64,
+    /// The associated registrant (contact) ID.
+    pub contact_id: u64,
+    // The extended attributes.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub extended_attributes: Option<HashMap<String, String>>,
+}
+
+/// Payload used to check the requirements for a contact change
+#[derive(Debug, Deserialize, Serialize)]
+pub struct RegistrantChangeCheckPayload {
+    /// The associated domain ID.
+    pub domain_id: u64,
+    /// The associated registrant (contact) ID.
+    pub contact_id: u64,
+}
+
+struct RegistrantChangeEndpoint;
+
+impl Endpoint for RegistrantChangeEndpoint {
+    type Output = RegistrantChange;
+}
+
+struct DeleteRegistrantChangeEndpoint;
+
+impl Endpoint for DeleteRegistrantChangeEndpoint {
+    type Output = Option<RegistrantChange>;
+}
+
+struct RegistrantChangesEndpoint;
+
+impl Endpoint for RegistrantChangesEndpoint {
+    type Output = Vec<RegistrantChange>;
+}
+
+struct RegistrantChangeCheckEndpoint;
+
+impl Endpoint for RegistrantChangeCheckEndpoint {
+    type Output = RegistrantChangeCheck;
+}
+
+impl Registrar<'_> {
+    /// Retrieve the domain contact change
+    ///
+    /// # Arguments
+    ///
+    /// `account_id`: The account ID
+    /// `registrant_change_id`: The contact change ID
+    pub fn get_registrant_change(
+        &self,
+        account_id: u64,
+        registrant_change_id: u64,
+    ) -> Result<DNSimpleResponse<RegistrantChange>, DNSimpleError> {
+        let path = format!(
+            "/{}/registrar/registrant_changes/{}",
+            account_id, registrant_change_id
+        );
+
+        self.client.get::<RegistrantChangeEndpoint>(&path, None)
+    }
+
+    /// Retrieves the requirements of a registrant change
+    ///
+    /// # Arguments
+    ///
+    /// `account_id`: The account ID
+    /// `payload`: The `RegistrantChangeCheckPayload` with the information needed to check the
+    /// requirements for a registrant change
+    pub fn check_registrant_change(
+        &self,
+        account_id: u64,
+        payload: RegistrantChangeCheckPayload,
+    ) -> Result<DNSimpleResponse<RegistrantChangeCheck>, DNSimpleError> {
+        let path = format!("/{}/registrar/registrant_changes/check", account_id);
+
+        match serde_json::to_value(payload) {
+            Ok(json) => self
+                .client
+                .post::<RegistrantChangeCheckEndpoint>(&path, json),
+            Err(_) => Err(DNSimpleError::Deserialization(String::from(
+                "Cannot deserialize json payload",
+            ))),
+        }
+    }
+
+    /// Start registrant change.
+    ///
+    /// # Arguments
+    ///
+    /// `account_id`: The account ID
+    /// `payload`: The `RegistrantChangePayload` with the information needed to start a registrant change
+    pub fn create_registrant_change(
+        &self,
+        account_id: u64,
+        payload: RegistrantChangePayload,
+    ) -> Result<DNSimpleResponse<RegistrantChange>, DNSimpleError> {
+        let path = format!("/{}/registrar/registrant_changes", account_id);
+
+        match serde_json::to_value(payload) {
+            Ok(json) => self.client.post::<RegistrantChangeEndpoint>(&path, json),
+            Err(_) => Err(DNSimpleError::Deserialization(String::from(
+                "Cannot deserialize json payload",
+            ))),
+        }
+    }
+
+    /// List registrant changes in the account.
+    ///
+    /// # Arguments
+    ///
+    /// `account_id`: The account ID
+    /// `options`: The `RequestOptions`
+    ///             - Filters: `domain_id`, `state`, `contact_id`
+    ///             - Sorting: `id`
+    pub fn list_registrant_changes(
+        &self,
+        account_id: u64,
+        options: Option<RequestOptions>,
+    ) -> Result<DNSimpleResponse<Vec<RegistrantChange>>, DNSimpleError> {
+        let path = format!("/{}/registrar/registrant_changes", account_id);
+
+        self.client.get::<RegistrantChangesEndpoint>(&path, options)
+    }
+
+    /// Cancel a registrant change.
+    ///
+    /// # Arguments
+    ///
+    /// `account_id`: The account ID
+    /// `registrant_change_id`: The contact change ID
+    pub fn delete_registrant_change(
+        &self,
+        account_id: u64,
+        registrant_change_id: u64,
+    ) -> Result<DNSimpleResponse<Option<RegistrantChange>>, DNSimpleError> {
+        let path = format!(
+            "/{}/registrar/registrant_changes/{}",
+            account_id, registrant_change_id
+        );
+
+        self.client
+            .delete_with_response::<DeleteRegistrantChangeEndpoint>(&path)
+    }
+}
---
+++ tests/registrar_registrant_changes_test.rs
@@ -0,0 +1,199 @@
+use crate::common::setup_mock_for;
+use dnsimple::dnsimple::registrar_registrant_changes::{
+    RegistrantChangeCheckPayload, RegistrantChangePayload,
+};
+mod common;
+use std::collections::HashMap;
+
+#[test]
+fn get_registrant_change_test() {
+    let setup = setup_mock_for(
+        "/101/registrar/registrant_changes/101",
+        "getRegistrantChange/success",
+        "GET",
+    );
+    let client = setup.0;
+    let account_id = 101;
+    let registrant_change_id = 101;
+
+    let response = client
+        .registrar()
+        .get_registrant_change(account_id, registrant_change_id)
+        .unwrap();
+    let registrant_change = response.data.unwrap();
+
+    assert_eq!(101, registrant_change.id);
+    assert_eq!(101, registrant_change.account_id);
+    assert_eq!(101, registrant_change.domain_id);
+    assert_eq!(101, registrant_change.contact_id);
+    assert_eq!("new", registrant_change.state);
+    assert_eq!(
+        HashMap::new(),
+        registrant_change.extended_attributes.unwrap()
+    );
+    assert!(registrant_change.registry_owner_change);
+    assert_eq!(None, registrant_change.irt_lock_lifted_by);
+
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.created_at);
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.updated_at);
+}
+
+#[test]
+fn check_registrant_change_test() {
+    let setup = setup_mock_for(
+        "/101/registrar/registrant_changes/check",
+        "checkRegistrantChange/success",
+        "POST",
+    );
+    let client = setup.0;
+    let account_id = 101;
+    let domain_id = 101;
+    let contact_id = 101;
+    let payload = RegistrantChangeCheckPayload {
+        domain_id,
+        contact_id,
+    };
+
+    let response = client
+        .registrar()
+        .check_registrant_change(account_id, payload)
+        .unwrap();
+    let registrant_change = response.data.unwrap();
+
+    assert_eq!(101, registrant_change.contact_id);
+    assert_eq!(101, registrant_change.domain_id);
+    assert_eq!(0, registrant_change.extended_attributes.unwrap().len());
+    assert!(registrant_change.registry_owner_change);
+}
+
+#[test]
+fn create_registrant_change_test() {
+    let setup = setup_mock_for(
+        "/101/registrar/registrant_changes",
+        "createRegistrantChange/success",
+        "POST",
+    );
+    let client = setup.0;
+    let account_id = 101;
+    let domain_id = 101;
+    let contact_id = 101;
+    let payload = RegistrantChangePayload {
+        domain_id,
+        contact_id,
+        extended_attributes: Option::Some(HashMap::from([(
+            String::from("foo"),
+            String::from("bar"),
+        )])),
+    };
+
+    let response = client
+        .registrar()
+        .create_registrant_change(account_id, payload)
+        .unwrap();
+    let registrant_change = response.data.unwrap();
+
+    assert_eq!(101, registrant_change.id);
+    assert_eq!(101, registrant_change.account_id);
+    assert_eq!(101, registrant_change.domain_id);
+    assert_eq!(101, registrant_change.contact_id);
+    assert_eq!("new", registrant_change.state);
+    assert_eq!(
+        HashMap::new(),
+        registrant_change.extended_attributes.unwrap()
+    );
+    assert!(registrant_change.registry_owner_change);
+    assert_eq!(None, registrant_change.irt_lock_lifted_by);
+
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.created_at);
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.updated_at);
+}
+
+#[test]
+fn list_registrant_changes_test() {
+    let setup = setup_mock_for(
+        "/101/registrar/registrant_changes",
+        "listRegistrantChanges/success",
+        "GET",
+    );
+    let client = setup.0;
+    let account_id = 101;
+
+    let response = client
+        .registrar()
+        .list_registrant_changes(account_id, None)
+        .unwrap();
+    let registrant_changes = response.data.unwrap();
+    let pagination = response.pagination.unwrap();
+
+    assert_eq!(1, pagination.total_entries);
+
+    assert_eq!(1, registrant_changes.len());
+
+    let registrant_change = &registrant_changes[0];
+    assert_eq!(101, registrant_change.id);
+    assert_eq!(101, registrant_change.account_id);
+    assert_eq!(101, registrant_change.domain_id);
+    assert_eq!(101, registrant_change.contact_id);
+    assert_eq!("new", registrant_change.state);
+    assert_eq!(
+        &HashMap::new(),
+        registrant_change.extended_attributes.as_ref().unwrap()
+    );
+    assert!(registrant_change.registry_owner_change);
+    assert_eq!(None, registrant_change.irt_lock_lifted_by);
+
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.created_at);
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.updated_at);
+}
+
+#[test]
+fn delete_registrant_change_test() {
+    let setup = setup_mock_for(
+        "/101/registrar/registrant_changes/101",
+        "deleteRegistrantChange/success",
+        "DELETE",
+    );
+    let client = setup.0;
+    let account_id = 101;
+    let registrant_change_id = 101;
+
+    let response = client
+        .registrar()
+        .delete_registrant_change(account_id, registrant_change_id);
+
+    // assert!(response.is_ok());
+    assert_eq!(204, response.unwrap().status);
+}
+
+#[test]
+fn delete_registrant_change_async_response_test() {
+    let setup = setup_mock_for(
+        "/101/registrar/registrant_changes/101",
+        "deleteRegistrantChange/success_async",
+        "DELETE",
+    );
+    let client = setup.0;
+    let account_id = 101;
+    let registrant_change_id = 101;
+
+    let response = client
+        .registrar()
+        .delete_registrant_change(account_id, registrant_change_id)
+        .unwrap();
+    let registrant_change = response.data.unwrap().unwrap();
+
+    assert_eq!(101, registrant_change.id);
+    assert_eq!(101, registrant_change.account_id);
+    assert_eq!(101, registrant_change.domain_id);
+    assert_eq!(101, registrant_change.contact_id);
+    assert_eq!("cancelling", registrant_change.state);
+    assert_eq!(
+        &HashMap::new(),
+        registrant_change.extended_attributes.as_ref().unwrap()
+    );
+    assert!(registrant_change.registry_owner_change);
+    assert_eq!(None, registrant_change.irt_lock_lifted_by);
+
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.created_at);
+    assert_eq!("2017-02-03T17:43:22Z", registrant_change.updated_at);
+}
